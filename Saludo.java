960e7079aa37ea99fb9ac695785912197ef4888c
AvrilAcosta <axaa0007@ce.pucmm.edu.do>
2026-05-29 16:24:32 -0400

    Update greting message

--- Saludo.java
+++ Saludo.java
@@ -1,6 +1,6 @@
 // Java clásico (Java 8+)
 public class Saludo {
     public static void main(String[] args) {
-        System.out.println("Hola, Git!");
+        System.out.println("Hola, Git! Ya entiendo los commits.");
     }
 }
