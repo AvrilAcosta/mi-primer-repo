45e458eb4eeff126ec4bf190dd269dad50d9fa1b
AvrilAcosta <axaa0007@ce.pucmm.edu.do>
2026-05-29 16:30:38 -0400

    Add staging area message to Saludo

--- Saludo.java
+++ Saludo.java
@@ -2,8 +2,6 @@
 public class Saludo {
     public static void main(String[] args) {
         System.out.println("Hola, Git! Ya entiendo los commits.");
-        System.out.
-
-                println("Version: 1.1");
+        System.out.println("Ahora entiendo el staging area.");
     }
 }
