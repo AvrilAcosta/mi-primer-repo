1e8ffa78226b6137e7a9e2a6c6c0ca241cbee119
AvrilAcosta <axaa0007@ce.pucmm.edu.do>
2026-05-29 16:25:50 -0400

    Add version info to greeting

--- Saludo.java
+++ Saludo.java
@@ -2,5 +2,8 @@
 public class Saludo {
     public static void main(String[] args) {
         System.out.println("Hola, Git! Ya entiendo los commits.");
+        System.out.
+
+                println("Version: 1.1");
     }
 }
